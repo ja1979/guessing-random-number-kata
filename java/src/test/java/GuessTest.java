fa14a811d395fe911c5d21d6795fedb794bcad24
Jorge Aguilar Bravo <jorgeaguilar@softwareevolutivo.com.ec>
2022-10-06 11:46:55 -0500

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:java/src/test/java/GuessTest.java

---
+++ java/src/test/java/GuessTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class GuessTest {
+
+  @Test
+  public void testGeneratedNumber() {
+
+
+    Guess guess = new Guess();
+
+    
+
+  }
+
+}
